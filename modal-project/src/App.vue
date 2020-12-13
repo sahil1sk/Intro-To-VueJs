5cca1ca4ef1ae166b7f19208cae6994f6c4c9721
sahil1sk <khannasahil303@gmail.com>
2020-12-13 16:47:09 +0530

    More on Vue like teleporting etc

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,6 +1,37 @@
 <template>
   <h1>{{ title }}</h1>
-  <Modal />
+  <p>Welcome...</p>
+  
+  <div v-if="showModal">
+    <!-- @close this is the custom event which will emit from the Modal.vue file -->
+    <Modal 
+      :header=header 
+      :text=text
+      theme="sale"
+      @close="toggleModal" 
+    >
+      <!-- passing the name slot of name links-->
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more infor</a>
+      </template>
+    
+      <h1>Ninja Giveway</h1>
+      <p>Grap your ninja swag for half price</p>
+    </Modal>
+  </div>
+
+  <!--In teleport we place this teleport div inside modals div which is inside public/index.html file-->
+  <teleport to="#modals"  v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <h1>Sign up to the newsletter</h1>
+      <p>For updates and promo codes!</p>
+    </Modal>
+  </teleport>
+  
+  <!--click.right means only when right click click.shift means when shift key is down-->
+  <button @click.shift="toggleModal">Open modal (shift)</button>
+  <button @click="toggleModalTwo">Open modal</button>
 </template>
 
 <script>
@@ -13,14 +44,26 @@ export default {
   },
   data() {
     return {
-      title: 'My First Vue App :)'
+      title: 'My First Vue App :)',
+      header: 'Sign up for the Giveway!',
+      text: 'Grap your ninja swag for half price!',
+      showModal: false,
+      showModalTwo: false,
     }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
+    },
   }
 }
 </script>
 
 <style>
-#app {
+#app, #modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- modal-project/src/components/Modal.vue
+++ modal-project/src/components/Modal.vue
@@ -1,24 +1,98 @@
 <template>
-    <div class="backdrop">
-        <div class="modal">
-            <p>modal content</p>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{ sale: theme === 'sale' }"> 
+            <slot> 
+                defaul content
+            </slot> 
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
         </div>
     </div>
 </template>
 
+<script>
+// <template>
+//     <!--click.self means when only click on that div the event is fired-->
+//     <div class="backdrop" @click.self="closeModal">
+//         <div class="modal" :class="{ sale: theme === 'sale' }"> <!-- if theme is equal to sale then set sale class otherwise not-->
+//             <!-- <h1>{{ header }}</h1>
+//             <p>{{ text }}</p> -->
+//             <slot> <!-- if no content passed then it will show default content -->
+//                 defaul content
+//             </slot> <!--helps to set the data which we pass in the Modal while using-->
+//             <div class="actions">
+//                 <!-- setting the name slot if any there of links named -->
+//                 <slot name="links"></slot>
+//             </div>
+//         </div>
+//     </div>
+// </template>
+
+export default {
+    props: [
+        'header', 
+        'text', 
+        'theme',
+    ],
+    methods: {
+        closeModal() {
+            // here we emitting the custom event
+            this.$emit('close'); 
+        }
+    }
+}
+</script>
+
+
 <style>
-    .modal {
-        width: 400px;
-        padding: 20px;
-        margin: 100px;
-        background: white;
-        border-radius: 10px;
-    }
-    .backdrop {
-        top: 0;
-        position: fixed;
-        background: rgba(0,0,0,0.5);
-        width: 100%;
-        height: 100%;
-    }
+  .modal {
+    width: 400px;
+    padding: 20px;
+    margin: 200px auto;
+    background: white;
+    border-radius: 10px;
+  }
+  .backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+  }
+  .modal h1 {
+    color: #03cfb4;
+    border: none;
+    padding: 0;
+  }
+  .modal p {
+    font-style: normal;
+  }
+  .modal .actions {
+    text-align: center;
+    margin: 30px 0 10px 0;
+    color: #333;
+  }
+  .modal .actions a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+  }
+  /* sale styles */
+  .modal.sale {
+    background: crimson;
+    color: white;
+  }
+  .modal.sale h1 {
+    color: white;
+  }
+  .modal.sale .actions {
+    color: white;
+  }
+  .modal.sale .actions a {
+    color: white;
+  }
 </style>
