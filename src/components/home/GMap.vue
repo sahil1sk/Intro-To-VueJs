6cd01f91cdfe66c29c8f1b78df6c338d58f59c1e
sahil1sk <khannasahil303@gmail.com>
2020-12-24 07:20:05 +0530

    Adding google maps

---
+++ src/components/home/GMap.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="map">
+      <div class="google-map" id="map"></div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'GMap',
+    data() {
+        return {
+            lat: 53,
+            lng: -2
+        }
+    },
+    methods: {
+        renderMap() {
+            // google.maps.Map available backause of the script we place in our file
+            const map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: this.lat, lng: this.lng},
+                zoom: 6,
+                maxZoom: 15,
+                minZoom: 3,
+                streetViewControl: false
+            })
+        }
+    },
+    mounted() {
+        this.renderMap()
+    },
+}
+</script>
+
+<style>
+    .google-map{
+        height: 100%;
+        width: 100%;
+        margin: 0 auto;
+        background: #fff;
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: -1;
+    }
+</style>
---
+++ src/firebase/init.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase'
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyD0mKuOY5iL2YNdfYXJAQhVLD1GcfHQoO8',
+  authDomain: 'id-application-1.firebaseapp.com',
+  databaseURL: 'https://id-application-1.firebaseio.com',
+  projectId: 'id-application-1',
+  storageBucket: 'id-application-1.appspot.com',
+  messagingSenderId: '11687720573',
+  appId: '1:11687720573:web:28989f8cc38cdfa0b36314'
+}
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+// const messaging = firebase.messaging()
+
+// export {
+//   messaging,
+//   firebaseApp
+// }
+
+export default firebaseApp.firestore()
