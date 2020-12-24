0aa1fa9b70a344d7c5e208a06f53a6977821976a
sahil1sk <khannasahil303@gmail.com>
2020-12-24 07:45:06 +0530

    adding signup page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Signup from '../components/auth/Signup.vue'
 
 const routes = [
   {
@@ -7,6 +8,11 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  }
 ]
 
 const router = createRouter({
